68c11be031d537cb057284c0d27668e18c9dfefe
TimeDrapery <--help>
2020-11-12 18:06:14 -0800

    added week-3, day-2 work performed

---
+++ week-3/day-2/breakoutRooms/calculator.js
@@ -0,0 +1,44 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+
+function updateDisplay() {
+    // select the element with class of `calculator-screen`
+    const display = document.querySelector('.calculator-screen');
+    // update the value of the element with the contents of `displayValue`
+    display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+    // Access the clicked element
+    const { target } = event;
+
+    // Check if the clicked element is a button.
+    // If not, exit from the function
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if (target.classList.contains('operator')) {
+        console.log('operator', target.value);
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        console.log('decimal', target.value);
+        return;
+    }
+
+    if (target.classList.contains('all-clear')) {
+        console.log('clear', target.value);
+        return;
+    }
+
+    console.log('digit', target.value);
+});
