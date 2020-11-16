7fd36f5bc41209dedaaec1f0511ce1892cad2a95
TimeDrapery <--help>
2020-11-16 11:24:34 -0800

    Test moving console message

--- finalProject/scripts/app.js
+++ finalProject/scripts/app.js
@@ -2,9 +2,9 @@
 
 'use strict'
 
-console.log("javaJavaJava");
-
 let today = new Date();
 let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
+
+console.log("javaJavaJava");
